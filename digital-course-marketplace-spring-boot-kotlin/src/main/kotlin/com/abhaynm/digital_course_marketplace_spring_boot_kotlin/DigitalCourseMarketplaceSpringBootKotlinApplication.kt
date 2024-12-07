d20590be12f6e39de65b8b58f48f8e3575096bd3
abhaynm02 <abhaybhaskarnm02@gmail.com>
2024-12-07 19:33:02 +0530

    set-up the initial spring boot project

---
+++ digital-course-marketplace-spring-boot-kotlin/src/main/kotlin/com/abhaynm/digital_course_marketplace_spring_boot_kotlin/DigitalCourseMarketplaceSpringBootKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.abhaynm.digital_course_marketplace_spring_boot_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DigitalCourseMarketplaceSpringBootKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<DigitalCourseMarketplaceSpringBootKotlinApplication>(*args)
+}
---
+++ digital-course-marketplace-spring-boot-kotlin/src/test/kotlin/com/abhaynm/digital_course_marketplace_spring_boot_kotlin/DigitalCourseMarketplaceSpringBootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.abhaynm.digital_course_marketplace_spring_boot_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DigitalCourseMarketplaceSpringBootKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
